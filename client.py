8c4f250b950cb04187beb0d6130e1ebabd4efe3f
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2022-08-14 15:31:28 -0700

    fix ping filter

--- client.py
+++ client.py
@@ -7,12 +7,16 @@ import sys
 import threading
 import time
 from ipaddress import IPv4Network
-from typing import List
+from typing import List, Dict
+
+import requests
+from requests import get, ConnectTimeout
 
 IPv4_CIDR = 24  # should work on pretty much all nets
 LOGGER = logging.getLogger('netAutoFS-Client')
 logging.basicConfig(level=logging.INFO)
 LOGGER.setLevel(logging.INFO)
+DEFAULT_PROTOCOL_PORT = 7007  # ENSURE THIS MATCHES! (see server.py)
 
 
 def get_local_ip() -> (str, IPv4Network):
@@ -44,7 +48,7 @@ def ping_spam(net: IPv4Network, thread_count: int = 32) -> List[str]:
     :return: None.
     """
     WIN_COUNT = '-n {}'
-    NIX_COUNT = '-c {}'
+    NIX_COUNT = '-c {} -w 1'
     LOGGER.info('Generating list of IPs to ping...')
 
     hosts = [str(i) for i in net.hosts()]
@@ -62,12 +66,14 @@ def ping_spam(net: IPv4Network, thread_count: int = 32) -> List[str]:
             raise NotImplementedError(f'Platform {sys.platform} not supported. Sorry :(')
         LOGGER.debug(f'Pinging {target}...')
         try:
-            subprocess.check_output(shlex.split(cmd), shell=True)
+            out = subprocess.check_output(shlex.split(cmd), shell=True)
         except subprocess.CalledProcessError:
-            LOGGER.info(f'{target} is down.')
-        else:
-            LOGGER.info(f'{target} is up.')
-            alive.append(target)
+            return
+        decoded = out.decode('utf-8')
+        if 'Destination host unreachable' in decoded or 'Request timed out' in decoded or '100% packet loss' in decoded:
+            return
+        LOGGER.info(f'{target} is up.')
+        alive.append(target)
 
     def limit_threads(max_count: int, interval=0.1):
         while len(threads) > max_count:
@@ -89,12 +95,56 @@ def ping_spam(net: IPv4Network, thread_count: int = 32) -> List[str]:
     return alive
 
 
+# noinspection HttpUrlsUsage
+def check_host_is_a_server(target: str) -> (bool, str):
+    try:
+        req = get(f'http://{target}:{DEFAULT_PROTOCOL_PORT}/exists', timeout=0.2)
+        assert req.status_code == 200
+        content = req.text
+        assert content.startswith(f'netAutoFS-Server')
+        return True, ' '.join(content.split(' ')[1:])
+    except AssertionError:
+        return False, ''
+    except requests.ConnectionError:
+        return False, ''
+
+
+def check_for_servers(targets: List[str], thread_count: int = 32) -> Dict[str, str]:
+    alive = {}
+    threads = []
+
+    def task(host: str):
+        result, name = check_host_is_a_server(host)
+        if result:
+            LOGGER.info(f'{host} is a server ({name})')
+            alive[host] = name
+
+    def limit_threads(max_count: int, interval=0.1):
+        while len(threads) > max_count:
+            for thread in threads.copy():
+                if not thread.is_alive():
+                    threads.remove(thread)
+            time.sleep(interval)
+
+    for target in targets:
+        limit_threads(thread_count)
+        LOGGER.debug(f'Starting new thread for {target}...')
+        t = threading.Thread(target=task, args=(target,))
+        t.start()
+        threads.append(t)
+        LOGGER.debug(f'Thread started')
+
+    LOGGER.info('Finishing up')
+    limit_threads(0)
+    LOGGER.info(f'Done, alive: {len(alive)}')
+    return alive
+
+
 def main():
     local, net = get_local_ip()
     print(f'i am {local}')
     up_names = ping_spam(net)
-    lsd = [int(i.split('.')[3]) for i in up_names]
-    print(f'range: {min(lsd)} - {max(lsd)}')
+    up_names_and_uas = check_for_servers(up_names)
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import identification
 
 app = Flask(__name__)
 localid = identification.get_local_random_id(32)
+DEFAULT_PROTOCOL_PORT = 7007
+
 
 AGENT = f'netAutoFS-Server {socket.gethostname()} {localid}'
 
@@ -13,4 +15,4 @@ def exists():
     return AGENT
 
 
-app.run(host='0.0.0.0', port=8080, debug=True)
+app.run(host='0.0.0.0', port=DEFAULT_PROTOCOL_PORT, debug=True)
